ca8991cc68ae8c540b0ca6408b13198ecce35959
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2024-03-15 15:39:16 +0200

    creating list of numbers

--- lib/screens/numbers_page.dart
+++ lib/screens/numbers_page.dart
@@ -5,11 +5,58 @@ import '../models/number.dart';
 
 class NumbersPage extends StatelessWidget {
   const NumbersPage({super.key});
-  final Number one = const Number(
-    image: 'assets/images/numbers/number_one.png',
-    jpName: 'ichi',
-    enName: 'one',
-  );
+  final List<Number> numbers = const [
+    Number(
+      image: 'assets/images/numbers/number_one.png',
+      jpName: 'ichi',
+      enName: 'one',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_two.png',
+      jpName: 'Ni',
+      enName: 'two',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_three.png',
+      jpName: 'Mittsu',
+      enName: 'three',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_four.png',
+      jpName: 'Shi',
+      enName: 'four',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_five.png',
+      jpName: 'Go',
+      enName: 'five',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_six.png',
+      jpName: 'Roku',
+      enName: 'six',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_seven.png',
+      jpName: 'Sebun',
+      enName: 'seven',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_eight.png',
+      jpName: 'Hachi',
+      enName: 'eight',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_nine.png',
+      jpName: 'Kyū',
+      enName: 'nine',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_ten.png',
+      jpName: 'Jū',
+      enName: 'ten',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,14 +66,16 @@ class NumbersPage extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Item(number: one),
-          Item(number: one),
-          Item(number: one),
-          Item(number: one),
-          Item(number: one),
-          Item(number: one),
-          Item(number: one),
-          Item(number: one),
+          Item(number: numbers[0]),
+          Item(number: numbers[1]),
+          Item(number: numbers[2]),
+          Item(number: numbers[3]),
+          Item(number: numbers[4]),
+          Item(number: numbers[5]),
+          Item(number: numbers[6]),
+          Item(number: numbers[7]),
+          Item(number: numbers[8]),
+          Item(number: numbers[9]),
         ],
       ),
     );
