fbb9bc85ebaa23639fe8118cb79133a494a207bd
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2024-03-15 14:42:05 +0200

    number category

--- lib/screens/numbers_page.dart
+++ lib/screens/numbers_page.dart
@@ -5,6 +5,48 @@ class NumbersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Numbers'),
+        backgroundColor: const Color(0xff46322B),
+      ),
+      body: Container(
+        color: const Color(0xffFFA944),
+        height: 100,
+        child: Row(
+          children: [
+            Container(
+              color: const Color(0xFFFFF6DC),
+              child: Image.asset('assets/images/numbers/number_one.png'),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'ichi',
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
+                  Text(
+                    'one',
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(flex: 1),
+            const Padding(
+              padding: EdgeInsets.only(right: 16.0),
+              child: Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 30.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
