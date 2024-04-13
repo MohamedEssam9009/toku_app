8dc5d730131c5ad8eb6b11461b212c271a61c61e
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-04-13 22:17:14 +0200

    Refactor UI

--- lib/components/item.dart
+++ lib/components/item.dart
@@ -20,39 +20,53 @@ class ListItem extends StatelessWidget {
             color: const Color(0xFFFFF6DC),
             child: Image.asset(item.image!),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  item.jpName,
-                  style: const TextStyle(color: Colors.white, fontSize: 18.0),
-                ),
-                Text(
-                  item.enName,
-                  style: const TextStyle(color: Colors.white, fontSize: 18.0),
-                ),
-              ],
-            ),
-          ),
-          const Spacer(flex: 1),
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: IconButton(
-              icon: const Icon(
-                Icons.play_arrow,
-                color: Colors.white,
-                size: 28.0,
-              ),
-              onPressed: () async {
-                final player = AudioPlayer();
-                await player.play(AssetSource(item.sound));
-              },
-            ),
-          ),
+          Expanded(child: ItemInfo(itemModel: item)),
         ],
       ),
+    );
+  }
+}
+
+class ItemInfo extends StatelessWidget {
+  const ItemInfo({super.key, required this.itemModel});
+  final ItemModel itemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                itemModel.jpName,
+                style: const TextStyle(color: Colors.white, fontSize: 18.0),
+              ),
+              Text(
+                itemModel.enName,
+                style: const TextStyle(color: Colors.white, fontSize: 18.0),
+              ),
+            ],
+          ),
+        ),
+        const Spacer(flex: 1),
+        Padding(
+          padding: const EdgeInsets.only(right: 16.0),
+          child: IconButton(
+            icon: const Icon(
+              Icons.play_arrow,
+              color: Colors.white,
+              size: 28.0,
+            ),
+            onPressed: () async {
+              final player = AudioPlayer();
+              await player.play(AssetSource(itemModel.sound));
+            },
+          ),
+        ),
+      ],
     );
   }
 }
@@ -67,42 +81,8 @@ class PhrasesItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: color,
-      height: 100,
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  item.jpName,
-                  style: const TextStyle(color: Colors.white, fontSize: 18.0),
-                ),
-                Text(
-                  item.enName,
-                  style: const TextStyle(color: Colors.white, fontSize: 18.0),
-                ),
-              ],
-            ),
-          ),
-          const Spacer(flex: 1),
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: IconButton(
-              icon: const Icon(
-                Icons.play_arrow,
-                color: Colors.white,
-                size: 28.0,
-              ),
-              onPressed: () async {
-                final player = AudioPlayer();
-                await player.play(AssetSource(item.sound));
-              },
-            ),
-          ),
-        ],
-      ),
+      height: 100.0,
+      child: ItemInfo(itemModel: item),
     );
   }
 }
